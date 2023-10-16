6818793002fe11d3dd3ee259f8eb28851eb49763
Sinmbf <sinmbf123@gmail.com>
2023-10-16 14:05:52 +0545

    Update

--- iNotebook/backend/index.js
+++ iNotebook/backend/index.js
@@ -9,7 +9,11 @@ const app = express();
 const port = 5000;
 
 // MIddleware to fix the cors
-app.use(cors())
+app.use(cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+}))
 // Middleware to parse the  req body 
 app.use(express.json());
 
@@ -19,6 +23,6 @@ app.use('/api/notes', require("./routes/notes"))
 
 
 app.listen(port, () => {
-    console.log(`iNotebook backend listening on port https://inotebook-backend-beta.vercel.app`);
+    console.log(`iNotebook backend listening on port https://localhost:${port}`);
 })
 
